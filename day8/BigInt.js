65db6dcda84a6be7f70ba451ca386a5a28f6a967
shashank kullkarni <shashankkulkarni80425@gmail.com>
2021-06-29 00:54:52 +0530

    pushing day 8 implimentarion and tasks

---
+++ day8/BigInt.js
@@ -0,0 +1,10 @@
+var bigNum = BigInt(
+    "123422222222222222222222222222222222222");
+  console.log(bigNum);
+
+  var bigHex = BigInt("0x1ffffffeeeeeeeeef");
+console.log(bigHex);
+
+var bigBin = BigInt(
+    "0b1010101001010101001111111111111111");
+  console.log(bigBin);
